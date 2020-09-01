5819705ce4c63bc71b462b227318125af8161ec2
ANA TULEA <anatulea14@gmail.com>
2020-09-01 14:23:30 -0700

    errors

---
+++ myprogram.py
@@ -0,0 +1,6 @@
+from mymodule import my_func
+from MyMainPackage import someMainScript 
+from MyMainPackage.SubPackage import mySubScript
+my_func()
+someMainScript.report_main()
+mySubScript.sub_report()
